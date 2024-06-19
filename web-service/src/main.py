d606896c30d4703a0755bbed2e3d3c8472de4418
gonzaloalonso-capilla <gonzaloalonso.capilla@vodafone.com>
2024-06-19 15:45:26 +0000

    status route + logs info

--- web-service/src/main.py
+++ web-service/src/main.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from config import app, matches_collection
-from models import createMatch
+from models import createMatch, getMatchStatus
 from utils.exceptions import APIError
 from utils.responses import Response
 
@@ -11,10 +11,26 @@ def hello_world():
 
 @app.route("/create", methods=["POST"])
 def post_create():
-    app.logger.info("[API] New /create request")
+    app.logger.info("[API] New POST /create request")
     match_id = createMatch()
-    app.logger.info("[API] /create went fine")
+    app.logger.info("[API] POST /create went fine")
     return Response(200, data={"matchId": match_id})
+
+
+@app.route("/status", methods=["GET"])
+def get_status():
+    app.logger.info("[API] New GET /status request")
+
+    matchId = request.json.get("matchId")
+
+    if not matchId or not isinstance(matchId, str):
+        app.logger.error("[API] GET /status missing correct matchId")
+        raise APIError(code=400, description="Bad Request")
+
+    result = getMatchStatus(matchId)
+
+    app.logger.info("[API] GET /status went fine")
+    return Response(200, data=result)
 
 if __name__== "__main__":
     app.run(debug=False,host='0.0.0.0')
