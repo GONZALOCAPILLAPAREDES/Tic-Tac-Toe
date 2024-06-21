31efc12e681f433a854d38b3a7fa2a884aa15f34
gonzaloalonso-capilla <gonzaloalonso.capilla@vodafone.com>
2024-06-21 11:02:16 +0000

    printing well APIErrors

--- web-service/src/utils/err_handler.py
+++ web-service/src/utils/err_handler.py
@@ -4,11 +4,13 @@ from utils.responses import Response
 from flask import current_app
 
 def err_handler(exception: Exception):
+
+    if hasattr(exception,"description") and hasattr(exception,"code"):
+        current_app.logger.error(f"{type(exception).__name__} - {exception.code}: {exception.description}")
+        return Response(exception.code, data={"error":exception.description})
+
     current_app.logger.error(f"{type(exception).__name__} - {exception}")
     if current_app.debug:
         current_app.logger.error(f"{type(exception).__name__} - {traceback.format_exc()}")
-
-    if exception.code and exception.description:
-        return Response(exception.code, data={"error":exception.description})
 
     return Response(500, data={"error":"Something went wrong"})
